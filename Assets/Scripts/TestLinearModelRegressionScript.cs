f59755087bd1bca91671412ff1c27bc45ec8544b
Kyoya27 <mouadh95@live.fr>
2020-05-12 15:23:35 +0200

    regresseion 1st

---
+++ Assets/Scripts/TestLinearModelRegressionScript.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TestLinearModelRegressionScript : MonoBehaviour
+{
+    
+
+
+    public Transform[] trainSpheresTransforms;
+
+    public Transform[] testSpheresTransforms;
+
+
+    public unsafe  void TrainAndTest()
+    {
+        Debug.Log("Training and Testing");
+
+    }
+}
--- Assets/Scripts/VisualStudioLibWrapper.cs
+++ Assets/Scripts/VisualStudioLibWrapper.cs
@@ -15,4 +15,7 @@ public static class VisualStudioLibWrapper
     
     [DllImport("ML_project")]
     public static extern void linear_model_train_classification(IntPtr model, double[] dataset_inputs, int dataset_length, int inputs_size, double[] dataset_expected_outputs, int outputs_size, int iterations_count, float alpha);
+
+    [DllImport("ML_project")]
+    public static extern void linear_model_train_regression(IntPtr model, double[] dataset_inputs, int dataset_length, int inputs_size, double[] dataset_expected_outputs, int outputs_size, int iterations_count, float alpha);
 }
