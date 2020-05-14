574f47ed12b65040fced264fc2838d39142becdd
Kyoya27 <mouadh95@live.fr>
2020-05-14 19:34:46 +0200

    rbf suite

--- Assets/Scripts/RBFClassScript.cs
+++ Assets/Scripts/RBFClassScript.cs
@@ -14,80 +14,19 @@ public class RBFClassScript : MonoBehaviour
     W
     x
     z*/
-    struct Projection
+    public struct Projection
     {
-        double W;
-        double[] X;
+        public double[] W;
+        public double[] X;
     }
 
     public Transform[] trainSpheresTransforms;
 
     public Transform[] testSpheresTransforms;
 
-    private int transfo = -1;
-
-    /*void   addCenterPoint(const double y, const std::vector<double>& x);
-    void   computeWeights(const bool useRegularization = false, const double lambda = 0.1);
-    double getInterpolatedValue(const std::vector<double>& x) const;
-    */
-    public Vector3 AppliTransfo(Vector3 position)
-    {
-        var posX = position.x;
-        var posZ = position.z;
-
-        //SOFT
-        if(transfo == 0){
-            if ((posX <= 5 && posX >= 4 && posZ >= 3 && posZ <= 4) || (posX >= -3 && posX <= -2 && posZ >= 9 && posZ <= 10))
-            {
-                posX += 5;
-                posZ += 5;
-            }
-
-            if (posX <= 4 && posX >= 3 && posZ >= 6 && posZ <= 7)
-            {
-                posX -= 5;
-                posZ -= 5;
-            }
-        }
-
-        //CROSS
-        if(transfo == 1){
-            posX = Math.Abs(position.x);
-            posZ = Math.Abs(position.z);
-            if (posX > 3 && posZ > 3 )
-            {
-                posX += 3f;
-                posZ += 3f;
-            }
-        }
-
-        //XOR
-        if(transfo == 2){
-            posX = (float)Math.Pow(position.x + position.z, 2);
-            posZ = (float)Math.Pow(position.x + position.z, 2);
-        }
-        return new Vector3(posX, position.y, posZ);
-    }
-
-
-    public void TransfoSoftThenTrain()
-    {
-        transfo = 0;
-    }
-
-    public void TransfoCrossThenTrain()
-    {
-        transfo = 1;
-    }
-
-    public void TransfoXORThenTrain()
-    {
-        transfo = 2;
-    }
-
     public double getRBFValue(double epsilon, double x1, double x2)
     {
-        return Math.exp(- epsilon *  Math.pow(Math.Abs(x1 - x2), 2.0));
+        return Math.Exp(- epsilon *  Math.Pow(Math.Abs(x1 - x2), 2.0));
     }
 
 
@@ -113,9 +52,20 @@ public class RBFClassScript : MonoBehaviour
         Matrix<double> OxInv = OxMatrix.Inverse();
         Matrix<double> OzInv = OzMatrix.Inverse();
 
+        
+        var X = new double[trainSpheresTransforms.Length * trainSpheresTransforms.Length *2];
+        var count = 0;
+        for (int i = 0; i < trainSpheresTransforms.Length; i++)
+        {
+            for (int j = 0; j < trainSpheresTransforms.Length; j++)
+            {
+                X[count] = OxMatrix[i, j]; 
+                X[count+1] = OzMatrix[i, j];
+                count = count+2;
+            }
+        }
 
-
-        return 
+        return X;
 
     }
 
@@ -138,14 +88,9 @@ public class RBFClassScript : MonoBehaviour
 
         for (int i = 0; i < trainSpheresTransforms.Length; i++)
         {
-            var position = new Vector3();
-            if(transfo != -1)
-                position = AppliTransfo(trainSpheresTransforms[i].position);
-            else
-                position = trainSpheresTransforms[i].position;
-            Y[i] = position.y > 0 ? 1 : -1;
-            linear_inputs[i*2] = position.x;
-            linear_inputs[(i*2)+1] = position.z;
+            Y[i] = trainSpheresTransforms[i].position.y > 0 ? 1 : -1;
+            linear_inputs[i*2] = trainSpheresTransforms[i].position.x;
+            linear_inputs[(i*2)+1] = trainSpheresTransforms[i].position.z;
         }
 
         var matrix_inputs = RBFEntries(1, Y);
@@ -153,19 +98,14 @@ public class RBFClassScript : MonoBehaviour
         // Create Model
         model = VisualStudioLibWrapper.linear_model_create(trainSpheresTransforms.Length);
         // Train Model
-        VisualStudioLibWrapper.linear_model_train_classification(model, linear_inputs, trainSpheresTransforms.Length, 2, Y, Y.Length,1000000, 0.01f);
+        VisualStudioLibWrapper.linear_model_train_classification(model, matrix_inputs, trainSpheresTransforms.Length, 2, Y, Y.Length,1000000, 0.01f);
         //double* model = {-0.4, 0.4, 0.2};
         //double[] model = new double [] {-0.4, 0.4, 0.2};
 
         // For each testSphere : Predict 
         foreach (var testSpheres in testSpheresTransforms)
         {
-            var position = new Vector3();
-            if(transfo != -1)
-                position = AppliTransfo(testSpheres.position);
-            else
-                position = testSpheres.position;
-            double[]input = { position.x, position.z};
+            double[]input = { testSpheres.position.x, testSpheres.position.z};
             //double y = (float)(-model[1] / model[2] * testSpheresTransform.position.x - model[0] / model[2]);
             float y = (float)VisualStudioLibWrapper.linear_model_predict_classification(model, input, 2);
             testSpheres.position = new Vector3(
@@ -176,6 +116,6 @@ public class RBFClassScript : MonoBehaviour
         }
 
         // Delete Model
-        VisualStudioLibWrapper.clearArray(model);
+        //VisualStudioLibWrapper.clearArray(model);
     }
 }
